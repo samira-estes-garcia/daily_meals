41a30c036ab0d8d6c493d5ba9b7fdd8fb3ba1da9
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-03-30 16:57:06 -0500

    added second level functionalities to breakfast option

--- lib/daily_meals/api.rb
+++ lib/daily_meals/api.rb
@@ -1,8 +1,13 @@
 class API
 
     @@all_breakfast_recipes = []
+    @@all_breakfast_ingredients = []
+    @@all_breakfast_urls = []
+
     @@all_lunch_recipes = []
+
     @@all_dinner_recipes = []
+
     @@all_snack_recipes = []
 
     def self.get_breakfast_recipes
@@ -17,6 +22,32 @@ class API
    def self.all_breakfast_recipes
     @@all_breakfast_recipes
    end
+
+    def self.get_breakfast_ingredients
+        response = HTTParty.get('http://www.recipepuppy.com/api/?q=breakfast&format=xml')
+        data_collect = response.dig('recipes', 'recipe')  
+        ingredients = data_collect.map{ |hash| hash['ingredients'] }
+        ingredients.each do |ingredient|
+            @@all_breakfast_ingredients << "Ingredients: #{ingredient}"
+        end
+    end
+
+    def self.all_breakfast_ingredients 
+     @@all_breakfast_ingredients
+    end
+
+    def self.get_breakfast_urls
+        response = HTTParty.get('http://www.recipepuppy.com/api/?q=breakfast&format=xml')
+        data_collect = response.dig('recipes', 'recipe')  
+        ingredients = data_collect.map{ |hash| hash['href'] }
+        ingredients.each do |href|
+            @@all_breakfast_urls << "Recipe Link: #{href}"
+        end
+    end
+
+    def self.all_breakfast_urls 
+     @@all_breakfast_urls
+    end
 
    def self.get_lunch_recipes
     response = HTTParty.get('http://www.recipepuppy.com/api/?q=lunch&format=xml')
--- lib/daily_meals/cli.rb
+++ lib/daily_meals/cli.rb
@@ -6,7 +6,7 @@ class CLI
 
     def main_menu
         puts 'Welcome to Daily Meals!'
-        puts 'Please select an option to find recipes to cook! Or type exit to exit the program.'
+        puts 'Please select an option to find recipes to cook! Type "menu" to access the main menu or type "exit" to exit the program.'
 
         puts "1. Breakfast"
         puts "2. Lunch"
@@ -20,8 +20,9 @@ class CLI
             choice = gets.strip
             case choice
             when "1"
-                puts "Select a number to display ingredients and a link to the recipe."
+                puts "Select a number to display ingredients and a link to the recipe. Type back to go back to the breakfast menu."
                 self.breakfast_recipes
+                self.breakfast_details
             when "2"
                 puts "Select a number to display ingredients and a link to the recipe."
                 self.lunch_recipes
@@ -31,12 +32,10 @@ class CLI
             when "4"
                 puts "Select a number to display ingredients and a link to the recipe."
                 self.snack_recipes
+            when "menu"
+                self.start
             end
         end 
-
-        # if choice = '5'
-        #     puts "Please enter a number between 1 and 4, or type exit to exit the program."
-        # end
 
         if choice == "exit"
             puts "Thank you for using Daily Meals. Happy Cooking!"
@@ -45,7 +44,66 @@ class CLI
 
     def breakfast_recipes 
         API.get_breakfast_recipes
+        API.get_breakfast_ingredients
+        API.get_breakfast_urls
         puts API.all_breakfast_recipes
+    end
+
+    def breakfast_details
+        breakfast_input = gets.strip
+        recipes = API.all_breakfast_recipes
+        ingredients = API.all_breakfast_ingredients
+        recipe_url = API.all_breakfast_urls
+
+        unless breakfast_input == 'back'
+            case breakfast_input
+            when "1"
+                puts "Recipe Name: #{recipes[0]}"
+                puts ingredients[0]
+                puts recipe_url[0]
+            when "2"
+                puts "Recipe Name: #{recipes[1]}"
+                puts ingredients[1]
+                puts recipe_url[1]
+            when "3"
+                puts "Recipe Name: #{recipes[2]}"
+                puts ingredients[2]
+                puts recipe_url[2]
+            when "4"
+                puts "Recipe Name: #{recipes[3]}"
+                puts ingredients[3]
+                puts recipe_url[3]
+            when "5"
+                puts "Recipe Name: #{recipes[4]}"
+                puts ingredients[4]
+                puts recipe_url[4]
+            when "6"
+                puts "Recipe Name: #{recipes[5]}"
+                puts ingredients[5]
+                puts recipe_url[5]
+            when "7"
+                puts "Recipe Name: #{recipes[6]}"
+                puts ingredients[6]
+                puts recipe_url[6]
+            when "8"
+                puts "Recipe Name: #{recipes[7]}"
+                puts ingredients[7]
+                puts recipe_url[7]
+            when "9"
+                puts "Recipe Name: #{recipes[8]}"
+                puts ingredients[8]
+                puts recipe_url[8]
+            when "10"
+                puts "Recipe Name: #{recipes[9]}"
+                puts ingredients[9]
+                puts recipe_url[9]
+            end
+        end
+
+        if breakfast_input == "back"
+            puts "is working"
+            #self.breakfast_recipes
+        end
     end
 
     def lunch_recipes 
