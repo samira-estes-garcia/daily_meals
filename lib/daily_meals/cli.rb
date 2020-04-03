5870605d7890cc99a4dc3da6b1c3ff2599b6cd15
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-04-03 15:11:44 -0500

    made changes to all files and condensed code. made code dynamic and DRY

--- lib/daily_meals/cli.rb
+++ lib/daily_meals/cli.rb
@@ -1,51 +1,38 @@
 class CLI
 
     def start
-        self.main_menu
-    end
-
-    def main_menu
         puts " "
-        puts "********************************************************************************"
-        puts " "
-        puts 'Welcome to Daily Meals!'.center(85).colorize(:light_blue)
-        puts " "
-        puts "********************************************************************************"
-        puts " "
-        puts 'Please select an option to find recipes to cook!'.colorize(:light_blue)
-        puts "Type".colorize(:light_blue) + " menu ".colorize(:red) + "to access the main menu or".colorize(:light_blue) + " exit ".colorize(:red) + "to exit the program.".colorize(:light_blue)
-        puts " "
+        puts 'Getting Data...'.colorize(:red)
         API.get_breakfast_recipes
         API.get_lunch_recipes
         API.get_dinner_recipes
         API.get_snack_recipes
-        puts "1. Breakfast"
-        puts "2. Lunch"
-        puts "3. Dinner"
-        puts "4. Snack"
-        
-        puts " "
-        puts "Enter Choice Number: ".colorize(:light_blue)
-        puts " "
+        self.main_menu
+    end
+
+    def main_menu
+        self.intro_display
 
         choice = nil
         while choice != 'exit'
             choice = gets.strip
             case choice
             when "1"
-                Meal.choice_display("breakfast")
-                self.breakfast_recipes
+                self.choice_display
+                self.list_recipes("breakfast")
             when "2"
-                Meal.choice_display("lunch")
-                self.lunch_recipes
+                self.choice_display
+                self.list_recipes("lunch")
             when "3"
-                Meal.choice_display("dinner")
-                self.dinner_recipes
+                self.choice_display
+                self.list_recipes("dinner")
             when "4"
-                Meal.choice_display("snack")
-                self.snack_recipes
+                self.choice_display
+                self.list_recipes("snack")
             when "menu"
-                self.start
+                self.main_menu
+            when "meal"
+                self.list_recipes(@user_selected_meal_object.meal_type)
             when "exit"
                 puts " "
                 puts "Thank you for using Daily Meals. Happy Cooking!".colorize(:light_blue)
@@ -60,116 +47,61 @@ class CLI
 
     end
 
-    #breakfast
-    def breakfast_recipes 
-        puts "Breakfast Recipe Titles".colorize(:green)
+    def list_recipes(eating_time) 
+        puts "#{eating_time.capitalize} Recipe Titles".colorize(:green)
         puts " "
-        @meals = Meal.specific_meals("breakfast")
+        @meals = Meal.specific_meals(eating_time)
         @meals.each.with_index(1) do |meal, index| 
               puts "#{index}. #{meal.recipe_title}"
         end
         puts " "
-        self.breakfast_details
+        self.eating_time_details
     end
 
-    def breakfast_details
-        breakfast_input = nil
-        while breakfast_input != "back" && breakfast_input != "exit"
-            breakfast_input = gets.strip
-            Meal.meal_choices(breakfast_input, "breakfast")
-        end
-
-        if breakfast_input == "back" 
-            puts " "
-            self.breakfast_recipes
-        else breakfast_input == "exit"
-            self.start
-        end
-
+    def eating_time_details
+        input = nil
+            input = gets.strip
+            if input.to_i >= 11
+                puts " "
+                puts "That's not a valid command. Enter 1-10 to select a meal.".colorize(:red)
+                puts " "
+            elsif input == "menu"
+                self.main_menu
+            else
+                index = (input.to_i - 1)
+                @user_selected_meal_object = @meals[index]
+                Meal.meal_detail_display(@meals[index])
+                puts "Type".colorize(:light_blue) + " 'menu' ".colorize(:red) + "to select a different meal category. Type".colorize(:light_blue) + " 'meal' ".colorize(:red) + "to view another recipe.".colorize(:light_blue)
+                puts " "
+                @meals = nil
+            end
     end
 
-    #lunch 
-    def lunch_recipes 
-        puts "Lunch Recipe Titles".colorize(:green)
+    def intro_display
         puts " "
-        @meals = Meal.specific_meals("lunch") 
-        @meals.each.with_index(1) do |meal, index| 
-              puts "#{index}. #{meal.recipe_title}"
-        end
-        puts " " 
-        self.lunch_details
-    end
-
-    def lunch_details
-        lunch_input = nil
-        while lunch_input != "back" && lunch_input != "exit"
-            lunch_input = gets.strip
-            Meal.meal_choices(lunch_input, "lunch")
-        end
-
-        if lunch_input == "back"
-            puts " " 
-            self.lunch_recipes
-        else lunch_input == "exit"
-            self.start
-        end
-
-    end
-
-    #dinner
-    def dinner_recipes 
-        puts "Dinner Recipe Titles".colorize(:green)
+        puts "********************************************************************************"
         puts " "
-        @meals = Meal.specific_meals("dinner") 
-        @meals.each.with_index(1) do |meal, index| 
-              puts "#{index}. #{meal.recipe_title}"
-        end
+        puts 'Welcome to Daily Meals!'.center(85).colorize(:light_blue)
         puts " "
-        self.dinner_details 
+        puts "********************************************************************************"
+        puts " "
+        puts 'Please select an option to find recipes to cook!'.colorize(:light_blue)
+        puts "Type".colorize(:light_blue) + " 'menu' ".colorize(:red) + "to access the main menu or".colorize(:light_blue) + " 'exit' ".colorize(:red) + "to exit the program.".colorize(:light_blue)
+        puts " "
+        puts "1. Breakfast"
+        puts "2. Lunch"
+        puts "3. Dinner"
+        puts "4. Snack"
+        puts " "
+        puts "Enter Choice Number: ".colorize(:light_blue)
+        puts " "
     end
 
-    def dinner_details
-       dinner_input = nil
-        while dinner_input != "back" && dinner_input != "exit"
-            dinner_input = gets.strip
-            Meal.meal_choices(dinner_input, "dinner")
-        end
-
-        if dinner_input == "back" 
-            puts " "
-            self.dinner_recipes
-        else dinner_input == "exit"
-            self.start
-        end
-
-    end
-
-    #snack
-    def snack_recipes 
-        puts "Snack Recipe Titles".colorize(:green)
+    def choice_display
         puts " "
-        @meals = Meal.specific_meals("snack") 
-        @meals.each.with_index(1) do |meal, index| 
-              puts "#{index}. #{meal.recipe_title}"
-        end
+        puts "Select a number to display ingredients and a link to the recipe.".colorize(:light_blue)
+        puts "Type".colorize(:light_blue) + " 'menu' ".colorize(:red) + "to access the main menu.".colorize(:light_blue)
         puts " "
-        self.snack_details 
-    end
-
-    def snack_details
-        snack_input = nil
-        while snack_input != "back" && snack_input != "exit"
-            snack_input = gets.strip
-            Meal.meal_choices(snack_input, "snack")
-        end
-
-        if snack_input == "back" 
-            puts " "
-            self.snack_recipes
-        else snack_input == "exit"
-            self.start
-        end
-
     end
 
 end
--- lib/daily_meals/meal.rb
+++ lib/daily_meals/meal.rb
@@ -12,53 +12,19 @@ def initialize(meal_type, api_hash)
     @@all << self
 end
 
-def self.choice_display(meal_selection)
-    puts " "
-    puts "Select a number to display ingredients and a link to the recipe.".colorize(:light_blue)
-    puts "Type".colorize(:light_blue) + " back ".colorize(:red) + "to to the #{meal_selection} menu or".colorize(:light_blue) + " exit ".colorize(:red) + "to access the main menu.".colorize(:light_blue)
-    puts " "
-end
-
 def self.specific_meals(meal_selection)
     self.all.select{ |meal_object| meal_object.meal_type == meal_selection }
 end
 
-def self.recipe_title(meal_selection)
-    puts "Breakfast Recipe Titles".colorize(:green)
+def self.meal_detail_display(meal_obj)
+   
     puts " "
-    meals = self.specific_meals(meal_selection) 
-    meals.each.with_index(1) do |meal, index| 
-        puts "#{index}. #{meal.recipe_title}"
-    end
+    puts "Recipe Name: ".colorize(:green) + meal_obj.recipe_title
+    puts "Ingredients: ".colorize(:green) + meal_obj.ingredients
+    puts "Recipe Link: ".colorize(:green) + meal_obj.recipe_link
     puts " "
-end
+    puts " "
 
-def self.meal_choices(index_of_user_selection, meal_selection)
-
-    details = self.specific_meals(meal_selection)
-    input = (index_of_user_selection.to_i - 1)
-    details[input]
-
-    if input >= 12
-        puts " "
-        puts "That's not a valid command. Enter 1-10 to select a meal.".colorize(:red)
-        puts " "
-        self.recipe_title(meal_selection)
-    elsif index_of_user_selection == "back"
-        puts " "
-        self.recipe_title(meal_selection)
-    elsif index_of_user_selection == "exit"
-        puts ""
-    else
-        puts " "
-        puts "Recipe Name: ".colorize(:green) + details[input].recipe_title
-        puts "Ingredients: ".colorize(:green) + details[input].ingredients
-        puts "Recipe Link: ".colorize(:green) + details[input].recipe_link
-        puts " "
-        puts "Type".colorize(:light_blue) + " back ".colorize(:red) + "go to the breakfast menu or".colorize(:light_blue) + " exit ".colorize(:red) + "to access the main menu.".colorize(:light_blue)
-        puts " "
-    end
-    
 end
 
 def self.all
